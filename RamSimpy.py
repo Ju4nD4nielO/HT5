d22554d784ab1c766fc8e8247db96cd8fd190420
Giancarlo24278 <sag24278@uvg.edu.gt>
2025-02-28 12:00:34 -0600

    Arreglando errores de RAMSIMPY

--- RamSimpy.py
+++ RamSimpy.py
@@ -1,18 +1,27 @@
 
-import random #importamos random para generar numeros al azar
+import random  # Importamos random para generar números al azar
 import simpy
 
-bandera = True
-def RAM (env, RAM):
-    while bandera == True:
-        DuracionMemoriaRAM = random.randint(1,10)
-        print(RAM, 'Empieza a usar memoria %d' % env.now,' tiempo de espera ',DuracionMemoriaRAM)
+def RAM(env, nombre):
+    while True:
+        # Generar duración de RAM 
+        DuracionMemoriaRAM = random.randint(1, 10)
+        print(f"{nombre} empieza a usar memoria en el tiempo {env.now}, tiempo de espera {DuracionMemoriaRAM}")
         yield env.timeout(DuracionMemoriaRAM)
 
+        # Generar duración de quieto 
+        DuracionQuieto = random.randint(1, 2)
+        print(f"{nombre} se queda quieto en el tiempo {env.now}, tiempo de espera {DuracionQuieto}")
+        yield env.timeout(DuracionQuieto)
 
+        # Detener la simulación cuando llega a 25
+        if env.now > 25:
+            print("Se acabó el tiempo de simulación")
+            break  # Sale del bucle while
 
-#Simulacion Simpy
-env = simpy.Environment() 
+# Simulación con SimPy
+env = simpy.Environment()
 
-env.process(RAM('Memoria RAM',env))
-env.run(until=25)  #correr la simulacin hasta el tiempo = 15
+env.process(RAM(env, "Memoria RAM"))
+env.run(until=25)
+
