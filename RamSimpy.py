172b4d51cf8edea0073c3f390ff1629ccdff84f2
Juan Daniel Ordoñez <ord24979@uvg.edu.gt>
2025-03-04 09:53:04 -0600

    Se termino programa

--- RamSimpy.py
+++ RamSimpy.py
@@ -19,16 +19,33 @@ def procesos(env, numero, ram, cpu): #simula la vida de cada proceso
     print("Proceso: " + str(numero) + " obtiene " + str(RAM_necesaria) + " unidades de RAM (Ready)")
 
     # Se asigna numero de instrucciones al proceso y se pone ready
-    instrucciones_totales = random.randint(1, 10)
-    print("Proceso: " + str(numero) + " tiene " + str(instrucciones_totales) + " instrucciones por ejecutar")
+    instrucciones_tot = random.randint(1, 10)
+    print("Proceso: " + str(numero) + " tiene " + str(instrucciones_tot) + " instrucciones por ejecutar")
 
-    while instrucciones_totales > 0: #Ciclo de ejecucion de procesos
+    while instrucciones_tot > 0: #Ciclo de ejecucion de procesos
         with cpu.request() as req:
             yield req
-            print()
+            print(str(env.now) + ": " + str(numero) + " es atendido por el CPU (Running)")
 
+            instrucciones_ejecutadas = min(INST_CPU, instrucciones_tot)
+            yield env.timeout(1)  # Simular el tiempo de ejecución
+            instrucciones_tot -= instrucciones_ejecutadas
+            print(f"{env.now}: {numero} ejecuta {instrucciones_ejecutadas} instrucciones. Le quedan {instrucciones_tot}")
 
+            if instrucciones_tot == 0:
+                print(str(env.now) + ": Proceso : " + str(numero) + " ha terminado de ejecutar instrucciones (Terminated)")
+                break
 
+            else:
+                aleatorio = random.randint(1,2)
+
+                if aleatorio == 1: # El proceso pasa a waiting I/O
+                    print(str(env.now) + ": " + str(numero) + " se va a I/O (Waiting)")
+                    yield env.timeout(1) # Tiempo de espera
+                    print(str(env.now) + ": " + str(numero) + " regresa a estado (Ready)")
+
+                else: # El proceso regresa a ready para ser atendido por el CPU
+                    print(str(env.now) + ": " + str(numero) + " Regresa a estado (Ready)")
 
 
 def generador(env, ram, cpu, num_procesos=25): #Generador de procesos, el num_procesos define 
@@ -43,7 +60,7 @@ def generador(env, ram, cpu, num_procesos=25): #Generador de procesos, el num_pr
 # Simulación con SimPy
 env = simpy.Environment()
 ram = simpy.Container(env, init=100, capacity=100)
-cpu = simpy.Resource(env, capacity=1)
+cpu = simpy.Resource(env, capacity=1) # Capacity es: CPU con x numero de nucleos
 
 # Inicia la generacion de los procesos
 env.process(generador(env, ram, cpu))
